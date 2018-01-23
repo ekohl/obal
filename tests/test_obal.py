1e77617bc8f4e28e692dc175053343d78013aa0a
Evgeni Golov <evgeni@golov.de>
2018-01-23 12:42:05 +0100

    add more tests for the args

--- tests/test_obal.py
+++ tests/test_obal.py
@@ -38,9 +38,48 @@ def test_generate_ansible_args_testpackage():
     assert 'testpackage' in ansible_args
 
 
+def test_generate_ansible_args_verbose():
+    cliargs = ['--verbose', 'scratch', 'testpackage']
+    ansible_args = _test_generate_ansible_args(cliargs)
+    assert 'testpackage' in ansible_args
+    assert '-v' in ansible_args
+
+
+def test_generate_ansible_args_vvvv():
+    cliargs = ['-vvvv', 'scratch', 'testpackage']
+    ansible_args = _test_generate_ansible_args(cliargs)
+    assert 'testpackage' in ansible_args
+    assert '-vvvv' in ansible_args
+
+
+def test_generate_ansible_args_extravars():
+    cliargs = ['-e', 'var1=one', '-e', 'var2=two', 'scratch', 'testpackage']
+    ansible_args = _test_generate_ansible_args(cliargs)
+    assert 'testpackage' in ansible_args
+    assert '-e' in ansible_args
+    assert 'var1=one' in ansible_args
+    assert 'var2=two' in ansible_args
+    assert '-e var1=one -e var2=two' in ' '.join(ansible_args)
+
+
+def test_generate_ansible_args_step():
+    cliargs = ['--step', 'scratch', 'testpackage']
+    ansible_args = _test_generate_ansible_args(cliargs)
+    assert 'testpackage' in ansible_args
+    assert '--step' in ansible_args
+
+
 def test_generate_ansible_args_tags():
     cliargs = ['--tags', 'wait,download', 'scratch', 'testpackage']
     ansible_args = _test_generate_ansible_args(cliargs)
     assert 'testpackage' in ansible_args
     assert '--tags' in ansible_args
+    assert 'wait,download' in ansible_args
+
+
+def test_generate_ansible_args_skiptags():
+    cliargs = ['--skip-tags', 'wait,download', 'scratch', 'testpackage']
+    ansible_args = _test_generate_ansible_args(cliargs)
+    assert 'testpackage' in ansible_args
+    assert '--skip-tags' in ansible_args
     assert 'wait,download' in ansible_args
